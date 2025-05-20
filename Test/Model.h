30fcf6c200c46fda3f9b99cad2b40e2c0b8d9051
Andrew <defibrullator@gmail.com>
2025-05-20 20:40:30 +0800

    1

---
+++ Test/Model.h
@@ -0,0 +1,135 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include "Mesh.h"
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Model {
+public:
+    vector<Mesh> meshes;
+    string directory;
+
+    Model(string const& path)
+    {
+        loadModel(path);
+    }
+
+    void Draw(GLuint shaderProgram, float angle, float verticalOffset, float horizontalOffset)
+    {
+        glm::mat4 rotationMat = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        for (unsigned int i = 0; i < meshes.size(); i++)
+        {
+            glm::mat4 modelMat;
+            switch (i)
+            {
+            case 0:
+                modelMat = glm::mat4(1.0f);
+                break;
+            case 1: 
+                modelMat = glm::translate(rotationMat, glm::vec3(0.0f, verticalOffset, 0.0f));
+                break;
+            case 2:
+                modelMat = rotationMat;
+                break;
+            case 3: 
+                modelMat = rotationMat;
+                break;
+            case 4:
+                modelMat = glm::translate(glm::translate(rotationMat, glm::vec3(0.0f, verticalOffset, 0.0f)), glm::vec3(horizontalOffset, 0.0f, 0.0f));
+                break;
+            default:
+                modelMat = glm::mat4(1.0f);
+                break;
+            }
+
+            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(modelMat));
+            glm::mat3 normalMatrix = glm::transpose(glm::inverse(glm::mat3(modelMat)));
+            glUniformMatrix3fv(glGetUniformLocation(shaderProgram, "normalMatrix"), 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+            meshes[i].Draw(shaderProgram);
+        }
+    }
+
+private:
+    void loadModel(string const& path)
+    {
+        Assimp::Importer importer;
+        const aiScene* scene = importer.ReadFile(path,
+            aiProcess_Triangulate |
+            aiProcess_GenNormals |
+            aiProcess_JoinIdenticalVertices |
+            aiProcess_SortByPType);
+
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+            cout << "ERROR ASSIMP:" << importer.GetErrorString() << endl;
+            return;
+        }
+
+        directory = path.substr(0, path.find_last_of('/'));
+        processNode(scene->mRootNode, scene);
+    }
+
+    void processNode(aiNode* node, const aiScene* scene)
+    {
+        for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+            meshes.push_back(processMesh(mesh, scene));
+        }
+
+        for (unsigned int i = 0; i < node->mNumChildren; i++) {
+            processNode(node->mChildren[i], scene);
+        }
+    }
+
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene)
+    {
+        vector<Vertex> vertices;
+        vector<unsigned int> indices;
+
+        for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            Vertex vertex;
+            glm::vec3 vector;
+
+            vector.x = mesh->mVertices[i].x;
+            vector.y = mesh->mVertices[i].y;
+            vector.z = mesh->mVertices[i].z;
+            vertex.Position = vector;
+
+            if (mesh->HasNormals()) {
+                vector.x = mesh->mNormals[i].x;
+                vector.y = mesh->mNormals[i].y;
+                vector.z = mesh->mNormals[i].z;
+                vertex.Normal = vector;
+            }
+            else {
+                vertex.Normal = glm::vec3(0.0f, 0.0f, 0.0f);
+            }
+
+            vertices.push_back(vertex);
+        }
+
+        for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+            aiFace face = mesh->mFaces[i];
+            for (unsigned int j = 0; j < face.mNumIndices; j++)
+                indices.push_back(face.mIndices[j]);
+        }
+
+        return Mesh(vertices, indices);
+    }
+};
+
+#endif
